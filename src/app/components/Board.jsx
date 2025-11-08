c631559efb9f1ca95c6c69d1922ee2ff776073fd
Suhail Hasan Khan <shasankhan@usf.edu>
2025-11-08 15:53:43 -0500

    Started working on the main Board.jsx page.

---
+++ src/app/components/Board.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import TicketList from './TicketList';
+import StatusFilter from './StatusFilter';
+import PriorityFilter from './PriorityFilter';
+import SearchBox from './SearchBox';
+import MyQueueSummary from './MyQueueSummary';
+import StatusMessage from './StatusMessage';
